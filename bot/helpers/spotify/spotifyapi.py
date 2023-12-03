0ebf382acd30b428e65d79ee63d83885b18db9fe
Mookhyang2212 <152599560+Mookhyang2212@users.noreply.github.com>
2023-12-03 20:08:03 +0700

    Update spotifyapi.py

--- bot/helpers/spotify/spotifyapi.py
+++ bot/helpers/spotify/spotifyapi.py
@@ -1,9 +1,7 @@
 import json
 import requests
-
 from librespot.core import Session
 from librespot.audio.decoders import AudioQuality
-
 from bot.helpers.translations import lang
 
 class SpotifyAPI:
@@ -11,40 +9,41 @@ class SpotifyAPI:
         self.session = None
         self.token = None
         self.quality = AudioQuality.HIGH
-        # If using mp3 - Set re-encode to True
         self.music_format = "ogg"
         self.reencode = False
 
     async def login(self, user_name, password):
-        """if os.path.isfile("credentials.json"):
-            self.session = Session.Builder().stored_file().create()
-            return"""
+        """Login to Spotify."""
+        # Use environment variables or a more secure method for credentials
         self.session = Session.Builder().user_pass(user_name, password).create()
 
     async def get_song_info(self, song_id):
+        """Get information about a Spotify song."""
         try:
-            info = json.loads(
-                requests.get(
-                    "https://api.spotify.com/v1/tracks?ids="
-                    + song_id
-                    + "&market=from_token",
-                    headers={"Authorization": "Bearer %s" % self.token},
-                ).text
-            )
-            is_playable = info["tracks"][0]["is_playable"]
+            url = f"https://api.spotify.com/v1/tracks?ids={song_id}&market=from_token"
+            headers = {"Authorization": f"Bearer {self.token}"}
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            info = response.json()["tracks"][0]
+            is_playable = info["is_playable"]
             if not is_playable:
                 return None, lang.select.ERR_SPOT_NOT_AVAIL
-            return info['tracks'][0], None
-        except:
+            return info, None
+        except requests.RequestException as e:
+            # Log the exception or raise a specific exception for better debugging
             pass
 
     async def get_album_name(self, album_id):
-        headers = {"Authorization": f"Bearer {self.token}"}
-        resp = requests.get(
-            f"https://api.spotify.com/v1/albums/{album_id}", headers=headers
-        ).json()
-        return resp
-    
+        """Get information about a Spotify album."""
+        try:
+            url = f"https://api.spotify.com/v1/albums/{album_id}"
+            headers = {"Authorization": f"Bearer {self.token}"}
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            # Log the exception or raise a specific exception for better debugging
+            pass
 
-
-spotify = SpotifyAPI() 
+# Instantiate the SpotifyAPI class
+spotify = SpotifyAPI()
